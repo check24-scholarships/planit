e31b982e7fa46e7e168ef131ce76854bb3735529
1n5pire <finlay.noerrenberg.samper@gmail.com>
2021-07-25 00:56:30 +0200

    Create a basic working UI
    - Create the basic UI that lets you...
      - Create a plan / beet
      - Input the plants
    
    - Add a standard type for bounding boxes

---
+++ planit/ui/syntax_highlighted_text.py
@@ -0,0 +1,89 @@
+
+import tkinter as tk
+import re
+
+
+class SyntaxHighlightedText (tk.Text):
+    def __init__(self, root, **kwargs):
+        super(SyntaxHighlightedText, self).__init__(root, **kwargs)
+        # self.bind("<KeyRelease>", lambda e: self._highlight_text())
+        self.bind("<<Modified>>", self._on_true_change)
+
+        self.patterns = []
+        self.state = "None"
+
+    def _on_true_change(self, event):
+        # Prevent this event from being called twice
+        if self.edit_modified():
+            self._highlight_text()
+
+        self.edit_modified(False)
+
+    def _clear_highlighting(self):
+        # Clear the existing tags
+        for tag in self.tag_names():
+            self.tag_remove(tag, "1.0", tk.END)
+
+    def _highlight_text(self):
+        self._clear_highlighting()
+
+        text = self.get_text()
+        for pattern, tag in self.patterns:
+            cursor_pos = (1, 0)
+
+            def consume(t: str):
+                nonlocal cursor_pos
+
+                line, col = cursor_pos
+
+                if "\n" in t:
+                    col = len(t) - t.rfind("\n") - 1
+                    line += t.count("\n")
+                else:
+                    col += len(t)
+
+                cursor_pos = (line, col)
+
+            def cursor_to_str():
+                line, col = cursor_pos
+                return "{}.{}".format(line, col)
+
+            last_idx = 0
+            for match in re.finditer(pattern, text):
+                start_idx = match.start()
+
+                consume(text[last_idx:start_idx])
+                start_pos = cursor_to_str()
+
+                consume(match.group(0))
+                end_pos = cursor_to_str()
+                last_idx = start_idx + len(match.group(0))
+
+                self.tag_add(tag, start_pos, end_pos)
+
+    def _highlight_text_via_tk(self):
+        # Clear the existing tags
+        for tag in self.tag_names():
+            self.tag_remove(tag, "1.0", tk.END)
+
+        for pattern, tag in self.patterns:
+            self.mark_set("matchEnd", "1.0")
+
+            count = tk.IntVar()
+            while True:
+                start_index = self.search(pattern, "matchEnd", tk.END, count=count)
+
+                if start_index == "": break
+                # Pattern matches 0 characters
+                if count.get() == 0: break
+
+                self.mark_set("matchEnd", f"{start_index}+{count.get()}c")
+                self.tag_add(tag, start_index, "matchEnd")
+
+    def highlight(self, pattern, **style):
+        name = id(pattern)
+        self.tag_config(name, **style)
+        self.patterns.append((pattern, name))
+
+    def get_text(self):
+        return self.get("1.0", tk.END)
