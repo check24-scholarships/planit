9bb7a21d24ddebd7808a8257872fa6b3017ea2a3
jakeiridion <emil.ej.jeleskovic@gmail.com>
2021-08-16 21:13:47 +0200

    cleaned up the code and optimized it a little.

--- planit/crawler/data_formatter.py
+++ planit/crawler/data_formatter.py
@@ -12,6 +12,7 @@ class DataFormatter:
         applies all format methods on the columns list.
         """
         columns = self._format_html_code_to_plain_text(columns)
+        columns = self._remove_footnotes_and_examples(columns)
         columns = self._format_column_entry_strings(columns)
         return columns
 
@@ -19,25 +20,48 @@ class DataFormatter:
         """
         returns the columns list but with plain text instead of html code.
         """
-        return [self._remove_footnotes_and_examples(column.text.strip()) for column in columns]
+        return [column.text.strip() for column in columns]
 
-    def _remove_footnotes_and_examples(self, column_entry: str) -> str:
+    def _remove_footnotes_and_examples(self, columns: list) -> list:
         """
         The regular expression removes square brackets and their contents (Wikipedia footnotes) as well as regular brackets
         and their contents (Examples).
         The square brackets are replaced with a ',' because sometimes it was forgotten in the article. :(
         """
-        return re.sub(r"\((.*?)\)", "", re.sub(r"\[(.*?)]", ",", column_entry))
+        square_brackets_pattern = re.compile(r"\[[^]]*]")
+        brackets_pattern = re.compile(r"\([^)]*\)")
+        return [brackets_pattern.sub("", square_brackets_pattern.sub(",", column)) for column in columns]
 
     def _format_column_entry_strings(self, columns: list) -> list:
         """
         formats the entries of helps, helped by, attracts ,-Repels/+distracts and avoid strings to sets.
         """
         new_columns = self._lemmatize_names(columns[:2])
-        for column in columns[2:]:
-            new_columns.append(self._split_column_entries_to_sets(column))
+        new_columns += self._split_column_entries_to_set(columns[2:])
+        new_columns[2:] = self._strip_entries(new_columns[2:])
         new_columns[2:] = self._lemmatize_sets(new_columns[2:])
+        new_columns = self._format_lemmatized_words(new_columns)
         return new_columns
+
+    def _split_column_entries_to_set(self, columns: list) -> list:
+        """
+        split the column entries on the following values: ",", ";", ".", "/", "and", "or".
+        return a list with sets containing the seperated column entries.
+        """
+        split_pattern = re.compile(r"[,;.&\/]\s?|\s?\band\s|\sor\s")
+        new_columns = []
+        for column in columns:
+            if not column:
+                new_columns.append(set())
+                continue
+            new_columns.append(set(split_pattern.split(column)))
+        return new_columns
+
+    def _strip_entries(self, columns: list) -> list:
+        """
+        returns a list of sets witch contain stripped values. if the value is "" it is skipped.
+        """
+        return [set(entry.strip('-" ') for entry in column if entry) for column in columns]
 
     def _lemmatize_names(self, names: list) -> list:
         """
@@ -49,42 +73,19 @@ class DataFormatter:
         """
         calls _lemmatize_word for each word in a list of sets (Helps, Helped by, Attracts, -Repels/+distracts, Avoid)
         """
-        # if the column value is None and therefore not a set it doesn't call _lemmatize_word() but instead just inserts
-        # None in the final list.
         return [set(self._lemmatize_word(entry) for entry in column) for column in columns]
 
     def _lemmatize_word(self, word: str) -> str:
         """
         turns nouns singular
         """
-        return self._replace_joined_punctuation(
-            " ".join([word.lemma_ if word.pos_ == "NOUN" else word.text for word in self._nlp(word.lower())]))
+        return " ".join([word.lemma_ if word.pos_ == "NOUN" else word.text for word in self._nlp(word.lower())])
 
-    def _replace_joined_punctuation(self, word: str) -> str:
+    def _format_lemmatized_words(self, columns: list) -> list:
         """
-        fixes the joined string that spacy returns.
+        format the strings returned by _lemmatize_word().
         """
-        return word.replace(" , ", ", ")\
-            .replace(" 's ", "'s ")\
-            .replace(" . ", ". ")\
-            .replace(" .", ".")\
-            .replace(" - ", "-")
-
-    # TODO: maybe only return empty set if column is empty?
-    def _split_column_entries_to_sets(self, column: str) -> set:
-        """
-        splits the column string on multiple different separators and return them as a set or return None if column empty.
-        """
-        # If an empty string is passed return None
-        if not column:
-            return set()
-        separators = [" and ", " or ", ";", "/", ". "]
-        for separator in separators:
-            column = column.replace(separator, ",")
-        return self._strip_entries(column.split(","))
-
-    def _strip_entries(self, entries: list) -> set:
-        """
-        strips all list entries and returns the entries as a set. Removes all empty strings as well.
-        """
-        return set(entry.strip().rstrip(".").strip("-").strip('"') for entry in entries if entry)
+        sub_pattern = re.compile(r" ([-/]|\.$|'s |[.,] ) ?")
+        # noinspection PyTypeChecker
+        return [sub_pattern.sub(r"\1", column) for column in columns[:2]] + \
+               [set(sub_pattern.sub(r"\1", entry) for entry in column if entry) for column in columns[2:]]
