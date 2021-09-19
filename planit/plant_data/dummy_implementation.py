edd53e0f0a961576b64f7e2d9ea99b895eea355d
1n5pire <finlay.noerrenberg.samper@gmail.com>
2021-09-19 14:13:41 +0200

    Create dummy implementation for the plant_data
    Implements #18

---
+++ planit/plant_data/dummy_implementation.py
@@ -0,0 +1,31 @@
+
+from ..standard_types import Plant
+from typing import Dict
+import os
+from functools import lru_cache
+
+
+@lru_cache(500)
+def get_symbiosis_score(plant_a: Plant, plant_b: Plant) -> int:
+    return 0
+
+
+def get_all_plants() -> Dict[str, str]:
+    return all_plants
+
+
+# Load in the plant names from the ./plants.txt file
+
+all_plants = {}
+local_folder = os.path.dirname(__file__)
+plants_list_file = os.path.join(local_folder, "plants.txt")
+
+with open(plants_list_file, "r") as file:
+    for line in file.readlines():
+        line = line.strip()
+
+        is_comment = line.startswith("#")
+        if is_comment or not line:
+            continue
+
+        all_plants[line] = line
