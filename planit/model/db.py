a51ae13f3fa72df04eaf1fa0cf9807a31c629223
kosmat <kosmat@posteo.de>
2021-09-27 15:11:16 +0200

    Provide database related functions directly by the module with a database class

--- planit/model/db.py
+++ planit/model/db.py
@@ -1,77 +1,91 @@
 # -*- coding: utf-8 -*-
 
-"""
-Database
-
-Connect to a sqlite3 database and execute SQL statements.
-"""
-
-import log, logging
+import logging
 import sqlite3
+from .log import createLogger
 from typing import Any, Dict, Iterable, List, Tuple
 
-# Path to the db file
-path: str = '../../var/planit.db'
+class Db:
+    """ Connect to a sqlite3 database and execute SQL statements. """
 
-# Use logger
-# TODO: Type hint for logger
-logger = log.createLogger('db')
+    def __init__(self):
 
-# Connect to the db
-connection: sqlite3.Connection = sqlite3.connect(path)
+        # Path to the db file
+        path: str = 'var/planit.db'
 
-def execute_sql(sql: str, values: Iterable[Any] = []) -> List[Tuple[str, ...]]:
-    """ Secure execution of an SQL statement. """
+        # Use logger
+        # TODO: Type hint for logger
+        self._logger = createLogger('db')
 
-    try:
+        # Connect to the db
+        self._connection: sqlite3.Connection = sqlite3.connect(path)
 
-        # Execute the SQL statement
-        cursor: sqlite3.Cursor = connection.cursor()
-        cursor.execute(sql, values)
-        logging.debug("Execute '{}' with values '{}'".format(sql, values))
+        # Create missing tables
+        self._create_tables()
 
-        # Get rows
-        rows: List[Tuple[str, ...]] = cursor.fetchall()
+    def _execute_sql(
+        self, sql: str, values: Iterable[Any] = []) -> List[Tuple[str, ...]]:
+        """ Execute a SQL statement securely. """
 
-        connection.commit()
-        return rows
+        try:
 
-    except Exception as e:
+            # Execute the SQL statement
+            cursor: sqlite3.Cursor = self._connection.cursor()
+            cursor.execute(sql, values)
+            logging.debug("Execute '{}' with values '{}'".format(sql, values))
 
-        # Rollback on exception
-        logging.warning('Database rollback')
-        connection.rollback()
+            # Get rows
+            rows: List[Tuple[str, ...]] = cursor.fetchall()
 
-        # Reraise exception
-        raise e
+            self._connection.commit()
+            return rows
 
-def create_tables() -> None:
-    """ Create the table plants. """
+        except Exception as e:
 
-    sql: str = ('CREATE TABLE IF NOT EXISTS plants (alias VARCHAR(255), ' +
-                'common_name VARCHAR(255));')
-    execute_sql(sql)
+            # Rollback on exception
+            logging.warning('Database rollback')
+            self._connection.rollback()
 
-def add_plant(alias: str, common_name: str) -> None:
-    """ Add a plant to the model. """
+            # Reraise exception
+            raise e
 
-    sql: str = 'INSERT INTO plants VALUES (?, ?)'
-    values: List[str] = [alias, common_name]
-    execute_sql(sql, values)
+    def _create_tables(self) -> None:
+        """ Create the tables plants and symbioses. """
 
-def get_all_plants() -> Dict[str, str]:
-    """ Get all plants. """
+        sql_create: str = 'CREATE TABLE IF NOT EXISTS {} ({});'
+        tables: Dict[str, List[str]] = {
+            'plants':
+            ['alias VARCHAR(255) PRIMARY KEY', 'common_name VARCHAR(255)'],
+            'symbioses':
+            ['plant_a VARCHAR(255)', 'plant_b VARCHAR(255), score INTEGER']
+        }
 
-    # Get the common names
-    sql: str = 'SELECT alias, common_name FROM plants;'
-    rows: List[Tuple[str, ...]] = execute_sql(sql)
+        for name in tables:
+            self._execute_sql(sql_create.format(name, ', '.join(tables[name])))
 
-    # Sort the plants
-    plants: Dict[str, str] = dict()
-    for row in rows:
-        plants[row[0]] = row[1]
+    def add_plant(self, alias: str, common_name: str) -> None:
+        """ Add a plant to the db. """
 
-    return plants
+        sql: str = 'INSERT INTO plants VALUES (?, ?)'
+        values: List[str] = [alias, common_name]
+        self._execute_sql(sql, values)
 
-# Create missing tables
-create_tables()
+    def get_all_plants(self) -> Dict[str, str]:
+        """ Get all plants from the db. """
+
+        # Get the common names
+        sql: str = 'SELECT alias, common_name FROM plants;'
+        rows: List[Tuple[str, ...]] = self._execute_sql(sql)
+
+        # Sort the plants
+        plants: Dict[str, str] = dict()
+        for row in rows:
+            plants[row[0]] = row[1]
+
+        return plants
+
+    def get_symbiosis_score(self, plant_a: str, plant_b: str) -> int:
+        """ Get the symbiosis score of the plants a and b from the db. """
+
+        # Default 0
+        return 0
