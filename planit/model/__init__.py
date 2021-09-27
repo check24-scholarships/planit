aba8d463c7dc10e736a73507788cb9c863943e45
- <->
2021-09-27 15:11:16 +0200

    Provide database related functions directly by the module with a database class

--- planit/model/__init__.py
+++ planit/model/__init__.py
@@ -1,1 +1,22 @@
-# Packet recognition
+# -*- coding: utf-8 -*-
+
+from .db import Db
+from typing import Dict
+
+# Create db object
+db: Db = Db()
+
+
+# Provide functions
+
+def add_plant(alias: str, common_name: str) -> None:
+    """ Add a plant to the db. """
+    return db.add_plant(alias, common_name)
+
+def get_all_plants() -> Dict[str, str]:
+    """ Get all plants from the db. """
+    return db.get_all_plants()
+
+def get_symbiosis_score(plant_a: str, plant_b: str) -> int:
+    """ Get the symbiosis score of the plants a and b from the db. """
+    return db.get_symbiosis_score(plant_a, plant_b)
