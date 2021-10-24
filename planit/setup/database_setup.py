0bb9db0bc47895db4a22c170dd3c5d6a7d361b34
jakeiridion <emil.ej.jeleskovic@gmail.com>
2021-10-24 12:03:14 +0200

    changed add code.

--- planit/setup/database_setup.py
+++ planit/setup/database_setup.py
@@ -9,8 +9,11 @@ def create_db_file():
         add_plant(plant.common_name, plant.common_name)
 
     for plant in plants:
-        for help_plant in plant.helps.union(plant.helped_by):
-            add_symbiosis_score(help_plant, plant.common_name, 1)
+        for help_plant in plant.helps:
+            add_symbiosis_score(plant.common_name, help_plant, 1)
+
+        for helped_by_plant in plant.helped_by:
+            add_symbiosis_score(helped_by_plant, plant.common_name, 1)
 
 
 if __name__ == '__main__':
