080994bf236c932e74346671c64e398c3522b678
1n5pire <finlay.noerrenberg.samper@gmail.com>
2021-11-01 15:47:12 +0100

    Use lazy imports to reduce app startup time
    Closes #28

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 
+# Use lazy imports to reduce the startup time of the app
+import lazy_import
+for module in ["spacy", "fuzzyset2", "dacite", "planit.plant_data", "planit.plant_data.dummy", "toml",
+               "beautifulsoup4", "bs4", "requests", "tabulate", "planit.genetic", "planit.crawler"]:
+    lazy_import.lazy_module(module)
+
+
 from planit.ui.ui import run_app
 
 
